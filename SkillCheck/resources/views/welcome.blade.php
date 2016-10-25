09009e8f387442e5ea1468d91023986acd5dd8d3
markliebs <markliebs@me.com>
2016-10-24 22:37:16 -0400

    Updated formatting only

--- SkillCheck/resources/views/welcome.blade.php
+++ SkillCheck/resources/views/welcome.blade.php
@@ -1,59 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Skill Check!</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
+	<title>Skill Check!</title>
 
-        <!-- Styles -->
-            <link rel="stylesheet" href="/css/reset.css" type="text/css">
-            <link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
-            <link rel="stylesheet" href="/css/style.css" type="text/css">
+	<!-- Fonts -->
 
-
-    </head>
+	<!-- Styles -->
+	<link rel="stylesheet" href="/css/reset.css" type="text/css">
+	<link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
 
 
-    <body>
+</head>
 
-<div class ="jumbotron">
-<div class ="centered">
 
-<div class ="margin-bot">
-<h1><b>Product Form</b></h1>
-</div>
+<body>
 
-{!! Form::open(['url' => '/products', 'files' => true]) !!}
+	<div class="jumbotron">
+		<div class="centered">
 
-<div class="form-group">
-{!! Form::label('productName', 'Product Name:') !!}
-{!! Form::text('productName', null, array('class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-{!! Form::label('quantityinStock', 'Quantity:') !!}
-{!! Form::text('quantityinStock', null, array('class' => 'form-control')) !!}
-</div>
-<div class="form-group">
-{!! Form::label('priceperItem', 'Price Per Item:') !!}
-{!! Form::text('priceperItem', null, array('class' => 'form-control')) !!}
-</div>
-{!! Form::submit('Submit', array('class' => 'btn btn-success btn-margin-top')) !!}
-{!! Form::close() !!}
+			<div class="margin-bot">
+				<h1><b>Product Form</b></h1>
+			</div>
 
-</div>
-</div>
+			{!! Form::open(['url' => '/products', 'files' => true]) !!}
+
+			<div class="form-group">
+				{!! Form::label('productName', 'Product Name:') !!} {!! Form::text('productName', null, array('class' => 'form-control'))
+				!!}
+			</div>
+			<div class="form-group">
+				{!! Form::label('quantityinStock', 'Quantity:') !!} {!! Form::text('quantityinStock', null, array('class' => 'form-control'))
+				!!}
+			</div>
+			<div class="form-group">
+				{!! Form::label('priceperItem', 'Price Per Item:') !!} {!! Form::text('priceperItem', null, array('class' => 'form-control'))
+				!!}
+			</div>
+			{!! Form::submit('Submit', array('class' => 'btn btn-success btn-margin-top')) !!} {!! Form::close() !!}
+
+		</div>
+	</div>
 
 </body>
+
 </html>
 
 
 
+{{--
 
-{{-- 
+Listing instructions for ease of reference
+
 Create a webpage with a form that has the following text input fields: Product name, Quantity in stock, Price per item.
 
 The submitted data of the form should be saved in an XML / JSON file with valid XML / JSON syntax.
